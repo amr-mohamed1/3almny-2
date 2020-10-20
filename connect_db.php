ff3bca4198a11ec28c1bc33a33d452437ea4884d
Amr Mohamed <63131532+amr-mohamed1@users.noreply.github.com>
2020-10-20 23:20:21 +0200

    Update connect_db.php

--- connect_db.php
+++ connect_db.php
@@ -1,8 +1,9 @@
 <?php 
 
-$dsn = "mysql:host=localhost;dbname=3almny";
-$user = "root";
-$pass="";
+$dsn = "mysql:host=ec2-54-157-234-29.compute-1.amazonaws.com
+;dbname=ds65hkqg6qgjh";
+$user = "ljgeopkddocela";
+$pass="484872fc139e3297ee54d183582b96430c7b6a204a4489d65798e3011624a675";
 
 try{
     $con = new PDO($dsn , $user , $pass);
